c1c69e16970fc60f6275cb7602e6cedb4cd4d983
godbod <gkounkou@gmail.com>
2017-02-18 23:03:32 +0100

    re-committing travis file path7

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,8 +5,10 @@ local readonly current_dir=$PWD
 
 # install qt
 sudo apt-get -qq update
-sudo apt-get install -y git build-essential libpng-dev python3-dev libboost-all-dev  libgl1-mesa-dev lemon flex qt5-default ninja-build cmake
-sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5* cmake
+# For Qt 5.7
+# sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5* cmake
+sudo add-apt-repository -y ppa:beineri/opt-qt57-trusty
+sudo apt-get install -qq g++-6 qt57-meta-full libqt5svg5*
 
 # install gtest
 sudo apt-get install libgtest-dev
