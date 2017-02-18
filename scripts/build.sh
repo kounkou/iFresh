6796bed456fd89ca226eef4ca5ab00ed8f86fbbe
godbod <gkounkou@gmail.com>
2017-02-18 21:52:16 +0100

    re-committing travis file path3

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,5 @@
 
 # build the iFresh application
 qmake -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug -o Makefile ../iFresh/iFresh.pro
+make
+./iFresh
--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get -qq update
-sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module
+sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
