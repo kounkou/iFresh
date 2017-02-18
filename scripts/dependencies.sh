9be4fe015982e602909bb8440cf509b24c7e96af
godbod <gkounkou@gmail.com>
2017-02-18 22:00:53 +0100

    re-committing travis file path4

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# memorize the current dir
+local readonly current_dir=$PWD
+
+# install qt
 sudo apt-get -qq update
-sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
+sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5* cmake
+
+# install gtest
+sudo apt-get install libgtest-dev
+sudo apt-get install cmake
+
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib
+
+# restore the current dir
+cd ${current_dir}
