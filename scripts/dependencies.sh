305525c99660e3214ac261fb382694c12cda4ee3
godbod <gkounkou@gmail.com>
2017-02-19 01:47:39 +0100

    re-commiting travis file patch9

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # memorize the current dir
-local readonly current_dir=$PWD
+current_dir=$PWD
 
 # install qt
 sudo apt-get -qq update
 # For Qt 5.7
 sudo apt-get install -y qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
-sudo add-apt-repository -y ppa:beineri/opt-qt57-trusty
-sudo apt-get install -qq g++-6 qt57-meta-full
 
 # install gtest
 sudo apt-get install libgtest-dev
