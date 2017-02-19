a4047e8e9d09fa187801d3feda18d59e128ad969
godbod <gkounkou@gmail.com>
2017-02-19 16:17:34 +0100

    re-commiting travis file patch28

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,7 +6,7 @@ current_dir=$PWD
 # install qt
 - sudo apt-get update -qq
 # For Qt 5.x
-sudo apt-get install --yes qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
+# sudo apt-get install --yes qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
 
 # install gtest
 sudo apt-get install --yes libgtest-dev
