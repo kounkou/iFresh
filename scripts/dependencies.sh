727ebe56b837f00c06be2b609a7a97493606db74
godbod <gkounkou@gmail.com>
2017-02-19 14:47:43 +0100

    re-commiting travis file patch19

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,24 +6,24 @@ current_dir=$PWD
 # install qt
 sudo apt-get -qq upgrade
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo apt-get install -qq g++-5
+sudo apt-get install --yes -qq g++-5
 # For Qt 5.x
-sudo apt-get install -y qt5-qmake \
-                        qt5-default \
-                        qdbus-qt5 \
-                        qtquick1-5-dev \
-                        qtscript5-dev \
-                        qtbase5-dev \
-                        qtdeclarative5-dev \
-                        libqt5webkit5-dev \
-                        libsqlite3-dev \
-                        qttools5-dev-tools \
-                        libcanberra-gtk-module \
-                        libqt5svg5*
+sudo apt-get install --yes qt5-qmake \
+                           qt5-default \
+                           qdbus-qt5 \
+                           qtquick1-5-dev \
+                           qtscript5-dev \
+                           qtbase5-dev \
+                           qtdeclarative5-dev \
+                           libqt5webkit5-dev \
+                           libsqlite3-dev \
+                           qttools5-dev-tools \
+                           libcanberra-gtk-module \
+                           libqt5svg5*
 
 # install gtest
-sudo apt-get install libgtest-dev
-sudo apt-get install cmake
+sudo apt-get install --yes libgtest-dev
+sudo apt-get install --yes cmake
 
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
