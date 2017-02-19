3c572e4fc60c465dfe5e65e4f80eb25836c74947
godbod <gkounkou@gmail.com>
2017-02-19 16:41:32 +0100

    re-commiting travis file patch30

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -9,13 +9,13 @@ current_dir=$PWD
 # sudo apt-get install --yes qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
 
 # install gtest
-sudo apt-get install --yes libgtest-dev
 sudo apt-get install --yes cmake
-
+sudo apt-get install --yes libgtest-dev
 sudo cd /usr/src/gtest
 sudo cmake CMakeLists.txt
 sudo make
 sudo cp *.a /usr/lib
+
 
 # restore the current dir
 cd ${current_dir}
