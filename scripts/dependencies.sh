26658ec4ccd9f27829ee9dd367f791a2b5deafec
godbod <gkounkou@gmail.com>
2017-02-25 14:49:54 +0100

    new version for Automatic testing

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,12 +3,17 @@
 # memorize the current dir
 current_dir=$PWD
 
-# For Qt 5.x
-echo Downloading Qt
-docker pull garthk/qt-build:trusty-5.7.0
-docker run -t -i --rm -v $PWD:$PWD garthk/qt-build:trusty-5.7.0 bash
-
+echo 'Phase I.  Installing CMake...'
 sudo apt-get install --yes cmake
+
+echo 'Phase II. Installing Qt'
+if [ -f qt-5.7 ]; then
+   ./qt-5.7
+fi
+
+if [ -f qt-5.7.env ]; then
+   source qt-5.7.env
+fi
 
 # restore the current dir
 cd ${current_dir}
