61ad75977cbdb2fe8a57aa8d9561c8655778f57a
godbod <gkounkou@gmail.com>
2017-02-25 13:43:57 +0100

    Trying docker install

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,19 +3,12 @@
 # memorize the current dir
 current_dir=$PWD
 
-# install qt
-- sudo apt-get update -qq
 # For Qt 5.x
-# sudo apt-get install --yes qt5-qmake qt5-default qdbus-qt5 qtquick1-5-dev qtscript5-dev qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qttools5-dev-tools libcanberra-gtk-module libqt5svg5*
+echo Downloading Qt
+docker pull garthk/qt-build:trusty-5.7.0
+docker run -t -i --rm -v $PWD:$PWD garthk/qt-build:trusty-5.7.0 bash
 
-# install gtest
 sudo apt-get install --yes cmake
-# sudo apt-get install --yes libgtest-dev
-# sudo cd /usr/src/gtest
-# sudo cmake CMakeLists.txt
-# sudo make
-# sudo cp *.a /usr/lib
-
 
 # restore the current dir
 cd ${current_dir}
