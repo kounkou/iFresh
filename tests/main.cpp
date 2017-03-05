f541e32d00782c6cdaa66d4895771f93643efb72
godbod <gkounkou@gmail.com>
2017-03-05 16:27:31 +0100

    update test main with QGuiApplication

--- tests/main.cpp
+++ tests/main.cpp
@@ -39,14 +39,10 @@
 ****************************************************************************/
 
 #include "gtest/gtest.h"
-#include <QGuiApplication>
 
 int main(int argc, char *argv[])
 {
-   QGuiApplication app(argc, argv);
- 
    ::testing::InitGoogleTest(&argc, argv);
-   RUN_ALL_TESTS();
 
-   return app.exec();
+   return RUN_ALL_TESTS();
 }
